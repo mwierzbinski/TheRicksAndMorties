17685346aaeded98c1cbd054810f25ab1c357dc3
Deor <michal.wierzbinski@gmail.com>
2024-10-06 18:02:57 +0200

    Initial commit for SwiftUI MVVM implementation

---
+++ RickAndMorty-SwiftUI-MVVM/RickAndMorty-SwiftUI-MVVM/RickAndMorty_SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMorty_SwiftUI_MVVMApp.swift
+//  RickAndMorty-SwiftUI-MVVM
+//
+//  Created by Michal on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMorty_SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMorty-SwiftUI-MVVM/RickAndMorty-SwiftUI-MVVMTests/RickAndMorty_SwiftUI_MVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMorty_SwiftUI_MVVMTests.swift
+//  RickAndMorty-SwiftUI-MVVMTests
+//
+//  Created by Michal on 06/10/2024.
+//
+
+import Testing
+@testable import RickAndMorty_SwiftUI_MVVM
+
+struct RickAndMorty_SwiftUI_MVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
